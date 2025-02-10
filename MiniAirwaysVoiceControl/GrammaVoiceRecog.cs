9d2c4ca7311ac2f6634c219a49f32637dca0f2b0
Sunsui <Sunsui@CONTEXTCROSS-SU>
2025-02-10 20:29:39 +0800

    添加项目文件。

---
+++ MiniAirwaysVoiceControl/GrammaVoiceRecog.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Globalization;
+using System.Speech.Recognition;
+
+namespace SpeechRecognitionApp
+{
+    class GrammaVoiceRecog
+    {
+        SpeechRecognitionEngine recognizer;
+        public delegate void OnSpeechRecognizedHandler(string Text);
+        public event OnSpeechRecognizedHandler OnSpeechRecognized;
+
+        public delegate void OnEngineStateChangeHandler(bool Success);
+        public event OnEngineStateChangeHandler OnEngineStateChanged;
+
+        public void Init()
+        {
+            recognizer =
+              new SpeechRecognitionEngine(
+                new System.Globalization.CultureInfo("en-US")
+                );
+
+            // Create and load a dictation grammar.  
+            recognizer.LoadGrammar(new DictationGrammar());
+
+            // Add a handler for the speech recognized event.  
+            recognizer.SpeechRecognized +=
+              new EventHandler<SpeechRecognizedEventArgs>(recognizer_SpeechRecognized);
+
+            recognizer.RecognizeCompleted +=
+                new EventHandler<RecognizeCompletedEventArgs>(recognizer_RecognizeCompleted);
+
+            // Configure input to the speech recognizer.  
+            recognizer.SetInputToDefaultAudioDevice();
+        }
+
+
+        public void Init(string language, Grammar grammar)
+        {
+            recognizer = new SpeechRecognitionEngine(new CultureInfo(language));
+            if (grammar == null)
+            {
+                recognizer.LoadGrammar(new DictationGrammar());
+            }
+            else
+            {
+                recognizer.LoadGrammar(grammar);
+            }
+
+            // Add a handler for the speech recognized event.  
+            recognizer.SpeechRecognized +=
+              new EventHandler<SpeechRecognizedEventArgs>(recognizer_SpeechRecognized);
+
+            recognizer.RecognizeCompleted +=
+                new EventHandler<RecognizeCompletedEventArgs>(recognizer_RecognizeCompleted);
+
+            // Configure input to the speech recognizer.  
+            recognizer.SetInputToDefaultAudioDevice();
+        }
+
+        public void Start()
+        {
+            // Start asynchronous, continuous speech recognition.  
+            recognizer.RecognizeAsync(RecognizeMode.Multiple);
+        }
+
+        public void Stop()
+        {
+            recognizer.RecognizeAsyncStop();
+        }
+
+
+        // Handle the SpeechRecognized event.  
+        void recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            OnSpeechRecognized?.Invoke(e.Result.Text);
+        }
+        void recognizer_RecognizeCompleted(object? sender, RecognizeCompletedEventArgs e)
+        {
+            OnEngineStateChanged?.Invoke(e?.Error == null);
+        }
+    }
+}
---
+++ MiniAirwaysVoiceControl/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Diagnostics;
+using System.Speech.Recognition;
+using SpeechRecognitionApp;
+
+namespace MiniAirwaysVoiceControl
+{
+    public class Program
+    {
+        static GrammaVoiceRecog VoiceRecog;
+        static void Main(string[] args)
+        {
+            Start();
+
+            // Keep the console window open.  
+            while (true)
+            {
+                Console.ReadLine();
+            }
+        }
+
+        private static Grammar CreateGrammar()
+        {
+            Choices AirlineChoice = new Choices(new string[] { "Delta", "America", "American", "United", "Southwest" });
+            GrammarBuilder AirlineElement = new GrammarBuilder(AirlineChoice);
+            Choices FlightNumberChoices = new Choices(new string[] { "one", "two", "three", "tree", "four", "five", "six", "seven", "eight", "nine", "zero"});
+            GrammarBuilder FlightNumberElement = new GrammarBuilder(FlightNumberChoices, 2, 4);
+            GrammarBuilder AircraftElement = new GrammarBuilder();
+            AircraftElement.Append(AirlineElement);
+            AircraftElement.Append(new GrammarBuilder("Airlines", 0, 1));
+            AircraftElement.Append(new GrammarBuilder("Flight", 0, 1));
+            AircraftElement.Append(FlightNumberElement);
+            AircraftElement.Culture = new System.Globalization.CultureInfo("en-US");
+
+            return new Grammar(AircraftElement);
+
+        }
+
+        static void Start()
+        {
+            VoiceRecog = new GrammaVoiceRecog();
+            Grammar AircraftGrammar = CreateGrammar();
+            AircraftGrammar.Name = "Aircraft";
+            VoiceRecog.Init("en-US", AircraftGrammar);
+            //VoiceRecog.Init();
+            VoiceRecog.Start();
+            VoiceRecog.OnSpeechRecognized += Show;
+            VoiceRecog.OnEngineStateChanged += Restart;
+        }
+
+        static void Show(string result)
+        {
+            Console.WriteLine(result);
+            VoiceRecog.Stop();
+        }
+
+        static async void Restart(bool _)
+        {
+            Console.WriteLine("End");
+            await Task.Delay(1000);
+            VoiceRecog.Start();
+        }
+    }
+}
